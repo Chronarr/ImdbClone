055c0f598895f3c082d5ff4cd599cf9e9331fa39
Chronarr <Lodberg@gmail.com>
2023-03-15 20:16:00 +0100

    Added loading effect

---
+++ src/app/error.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import { useEffect } from 'react'
+
+export default function Error({ error, reset }) {
+    useEffect(() => {
+        console.log(error)
+    }, [error]);
+    return (
+        <div className='text-center mt-10'>
+            <h2>Something went wrong!</h2>
+            <button onClick={() => reset()} className="hover:text-amber-600">Try again</button>
+        </div>
+    )
+}
